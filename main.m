0f5756df3db531b312a4b75478aa4883eb5b375c
andreberta <bertarini45@gmail.com>
2017-04-03 12:58:03 +0200

    modified main

--- main.m
+++ main.m
@@ -1,22 +1,22 @@
 %% general 
-addpath(genpath('functions'));
+addpath(genpath('/functions'));
 
 patients_id = [1 , 2 , 3];
 visit_per_patient = [3 , 3 , 4];
 
 curv_type = 'thickness';
 resolutions = 1000;
-level = length(resolutions);
+level = length(resolution);
 fwhm = 0;
 
-w_dim = 8;
-p_dim = 3;
+%w_dim = 8;
+%p_dim = 3;
 
 data_path_fun = @path_local;
 save_path_fun = @save_path;
 
 
-for ii =2:length(patients_id) %skip patient 1
+for ii =2;%:length(patients_id) %skip patient 1
     
     id = patients_id(ii);
     visit_number = visit_per_patient(ii);
@@ -24,8 +24,8 @@ for ii =2:length(patients_id) %skip patient 1
     
     patient = load_patient(id,visit_number,data_path_fun,curv_type,fwhm,resolutions); 
     
-    patient = SC(patient,level);  
+   % patient = SC(patient,level);  
     
-    save_patient(patient,save_path_fun);
+    %save_patient(patient,save_path_fun);
 
 end
