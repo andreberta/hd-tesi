db92033f7b8f56a823caffc8d7735492d3665c1c
andreberta <bertarini45@gmail.com>
2017-04-29 15:14:34 +0200

    modifed prova_2

--- prova_2.m
+++ prova_2.m
@@ -48,9 +48,9 @@ for ii=1:length(regions_lh)
    %extract parc info
     parc_lh_img = surf_to_pyramid_rect(vertices_lh_new,aparc_lh,resolutions,1,0);    
     %reduced mask for region
-    mask_lh = parc_lh_img(up:end-down,up:end-down) == pos;
+    mask = parc_lh_img== pos;
     %patch number
-    patch_number = sum(mask_lh(:));
+    patch_number = sum(mask(:));
     
     whole_patches = zeros(psz^2,patch_number * length(visits));
     curr = 1;
@@ -63,11 +63,19 @@ for ii=1:length(regions_lh)
         img = surf_to_pyramid_rect(vertices_lh_new,v_curv_lh,resolutions,0,0);
 
         %extract patches from img
-        S = im2colstep(img,[psz,psz],[step,step]);
-        S = S(:,mask_lh);
+%         S = im2colstep(img,[psz,psz],[step,step]);
+%         S = S(:,mask_lh);
+        [S,~] = random_patches_2(img,psz,patch_number,~mask);
+        % remove black patches
+        mean_S = mean(S,1);
+        S = S(:,mean_S > 0);
+        %remove mean from patches
+        S = bsxfun(@minus,S,mean_S);
+        %store them
         whole_patches(:,curr:curr+patch_number-1) = S;
         curr = curr+patch_number;
     end
+    clear curr S img v_curv_lh visit_path mask parc_lh_img
     
     %compute 70%
     index_70 = round(length(whole_patches)*0.7);
@@ -97,73 +105,80 @@ for ii=1:length(regions_lh)
 
 end
 
- %% compute -rh
-% result_rh = cell(length(regions_rh),length(lambda));
-% for ii=1:length(regions_rh)
-%     disp(regions_rh{ii});
-%     %get pos info
-%     pos = parc2pos(regions_rh{ii});
-%     x_rot = parameter.rh{pos}.x_rot;
-%     y_rot = parameter.rh{pos}.y_rot;
-%     z_rot = parameter.rh{pos}.z_rot;
-%     R = makehgtform('xrotate',x_rot,'yrotate',y_rot,'zrotate',z_rot); 
-%     
-%     %rotate vertices
-%     vertices_rh_ = [vertices_rh,ones(length(vertices_rh),1)];
-%     vertices_rotated = (R * vertices_rh_')';
-%     vertices_rh_new = addSphericalCoord(vertices_rotated(:,1:3));
-%     
-%    %extract parc info
-%     parc_rh_img = surf_to_pyramid_rect(vertices_rh_new,aparc_rh,resolutions,1,0);    
-%     %reduced mask for region
-%     mask_rh = parc_rh_img(up:end-down,up:end-down) == pos;
-%     %patch number
-%     patch_number = sum(mask_rh(:));
-%     
-%     whole_patches = zeros(psz^2,patch_number * length(visits));
-%     curr = 1;
-%     for jj=visits
-%         disp(['Loading visit: ',num2str(jj)])
-%         % load curvature
-%         visit_path = path_local(patient_id,jj);
-%         [v_curv_rh] = load_mgh_file(visit_path,'thickness','rh',0,0);
-%         %interpolate to get images
-%         img = surf_to_pyramid_rect(vertices_rh_new,v_curv_rh,resolutions,0,0);
-% 
-%         %extract patches from img
+ % compute -rh
+result_rh = cell(length(regions_rh),length(lambda));
+for ii=1:length(regions_rh)
+    disp(regions_rh{ii});
+    %get pos info
+    pos = parc2pos(regions_rh{ii});
+    x_rot = parameter.rh{pos}.x_rot;
+    y_rot = parameter.rh{pos}.y_rot;
+    z_rot = parameter.rh{pos}.z_rot;
+    R = makehgtform('xrotate',x_rot,'yrotate',y_rot,'zrotate',z_rot); 
+    
+    %rotate vertices
+    vertices_rh_ = [vertices_rh,ones(length(vertices_rh),1)];
+    vertices_rotated = (R * vertices_rh_')';
+    vertices_rh_new = addSphericalCoord(vertices_rotated(:,1:3));
+    
+   %extract parc info
+    parc_rh_img = surf_to_pyramid_rect(vertices_rh_new,aparc_rh,resolutions,1,0);    
+    %reduced mask for region
+    mask = parc_rh_img == pos;
+    %patch number
+    patch_number = sum(mask(:));
+    
+    whole_patches = zeros(psz^2,patch_number * length(visits));
+    curr = 1;
+    for jj=visits
+        disp(['Loading visit: ',num2str(jj)])
+        % load curvature
+        visit_path = path_local(patient_id,jj);
+        [v_curv_rh] = load_mgh_file(visit_path,'thickness','rh',0,0);
+        %interpolate to get images
+        img = surf_to_pyramid_rect(vertices_rh_new,v_curv_rh,resolutions,0,0);
+
+        %extract patches from img
 %         S = im2colstep(img,[psz,psz],[step,step]);
-%         S = S(:,mask_rh);
-%         whole_patches(:,curr:curr+patch_number-1) = S;
-%         curr = curr+patch_number;
-%     end
-%     
-%     %compute 70%
-%     index_70 = round(length(whole_patches)*0.7);
-%     for jj=1:length(lambda)
-%         disp(['Computing for lambda: ',num2str(lambda(jj))])
-%         curr_labda = lambda(jj);
-%         
-%         %random permutation for index of patches
-%     	rand_index = randperm(length(whole_patches));
-% 
-%     	%take the first 70% for train (to learn D)
-%     	train_patch = whole_patches(:,rand_index(1:index_70));
-%     	%take the lasts 30% for test (compute mean reconstruction error)
-%     	test_patch = whole_patches(:,rand_index(index_70+1:end));
-%         
-%         %learn D
-%         D0 = randn(psz^2,round((psz^2)*dict_dim_mult));
-%         D= bpdndl(D0,train_patch,curr_labda);
-%         
-%         %test
-%         X = bpdn(D,test_patch,curr_labda);
-%         
-%         result_rh{ii,jj}.D = D;
-%         result_rh{ii,jj}.err = sqrt(sum((D*X-test_patch).^2,1));
-% 
-%     end
-% 
-% end
+%         S = S(:,mask_lh);
+        [S,~] = random_patches_2(img,psz,patch_number,~mask);
+        % remove black patches
+        mean_S = mean(S,1);
+        S = S(:,mean_S > 0);
+        %remove mean from patches
+        S = bsxfun(@minus,S,mean_S);
+        %store them
+        whole_patches(:,curr:curr+patch_number-1) = S;
+        curr = curr+patch_number;
+    end
+    
+    %compute 70%
+    index_70 = round(length(whole_patches)*0.7);
+    for jj=1:length(lambda)
+        disp(['Computing for lambda: ',num2str(lambda(jj))])
+        curr_labda = lambda(jj);
+        
+        %random permutation for index of patches
+    	rand_index = randperm(length(whole_patches));
+
+    	%take the first 70% for train (to learn D)
+    	train_patch = whole_patches(:,rand_index(1:index_70));
+    	%take the lasts 30% for test (compute mean reconstruction error)
+    	test_patch = whole_patches(:,rand_index(index_70+1:end));
+        
+        %learn D
+        D0 = randn(psz^2,round((psz^2)*dict_dim_mult));
+        D= bpdndl(D0,train_patch,curr_labda);
+        
+        %test
+        X = bpdn(D,test_patch,curr_labda);
+        
+        result_rh{ii,jj}.D = D;
+        result_rh{ii,jj}.err = sqrt(sum((D*X-test_patch).^2,1));
+
+    end
+
+end
 
 
 
